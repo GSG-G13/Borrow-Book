5dcc9beee6292bd0ba55e3170db9969eeae4af12
momenmarwan <momenalgaga82@gmail.com>
2023-04-11 23:24:20 +0300

    reslove the conflict

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
 const { login } = require("../controllers/sginup/login");
 const { signUp } = require("../controllers/sginup/sginup");
 const { addBook } = require("../controllers/addBooks");
-=======
 const { getBooks } = require('../controllers/getBooks');
-const { login } = require('../controllers/sginup/login');
-const { signUp } = require('../controllers/sginup/sginup');
 
->>>>>>> main
 
 const router = require("express").Router();
 
 router.post("/signUp", signUp);
-router.post("/login", login);
-router.post("/home/addBook", addBook);
-
-<<<<<<< HEAD
-module.exports = router;
-=======
-
-router.post("/signUp", signUp);
 router.post('/login', login);
 router.get('/getBooks',getBooks )
+router.post("/home/addBook", addBook);
 
 module.exports = router
->>>>>>> main
