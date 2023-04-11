9a685323af475f7138d3ff83e52a50981f526b3b
momenmarwan <momenalgaga82@gmail.com>
2023-04-12 00:27:25 +0300

    make abdualla function works

--- public/scripts/dom.js
+++ public/scripts/dom.js
@@ -62,9 +62,21 @@ addBtn.addEventListener("click", () => {
     document.body.style.overflow = "hidden";
 });
 saveBtn.addEventListener("click", () => {
-    popupForm.classList.remove("active");
-    contentHider.classList.remove("active");
-    document.body.style.overflow = "auto";
+    fetch('/addBook',{
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            author : authorInput.value,
+            image: bookImgInput.value,
+            bookName : bookNameInput.value
+        })
+    })
+    .then(() => {
+        console.log('The request has been added')
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 });
 cancelBtn.addEventListener("click", () => {
     popupForm.classList.remove("active");
