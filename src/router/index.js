89d94586a43b075e1a883c26fa57eb5d5c886f1e
adalah-sh <adalah.sh00@gmail.com>
2023-04-12 13:16:24 +0200

    add cookie parser and auth for add book and get books

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,31 @@ const { signUp } = require("../controllers/sginup/sginup");
 const { addBook } = require("../controllers/addBooks");
 const { getBooks } = require('../controllers/getBooks');
 const {logout} = require('../controllers/sginup/logout')
+const { verify } = require("jsonwebtoken");
+
 
 
 const router = require("express").Router();
 
+const cookieParser = require("cookie-parser");
+router.use(cookieParser());
+const authenticateUser = (req, res, next) => {
+  const token = req.cookies.tokenn;
+  if (!token) {
+    return res.redirect("/");
+  }
+  verify(token, "privateKey", (err, token_) => {
+    if (err) {
+      return res.redirect("/");
+    }
+    next();
+  });
+};
+
 router.post("/signup", signUp);
 router.post('/login', login);
-router.get('/getBooks',getBooks);
-router.post("/addBook", addBook);
+router.get("/getBooks", authenticateUser, getBooks);
+router.post("/addBook", authenticateUser, addBook);
 router.get('logout', logout);
 
 module.exports = router
